72e68a31c952fe3b18b252e78a662741509466fa
Marie Angelica Alzola <marieangelicaa@gmail.com>
2013-02-07 11:20:12 +0800

    tab client list

--- static/header.php
+++ static/header.php
@@ -4,9 +4,9 @@
 				<ul class="ul-menu <?php echo $_SESSION['clientid'] ? 'five' : '';?>">
 					<li><a href="index.php">Home</a></li>
 					<li><a href="company_profile.php">Company Profile</a></li>
-					<li><a href="client.list.php">Client List</a></li>
+					<li><a href="client.list.php">Lists</a></li>
 					<?php if(isset($_SESSION['clientid'])) { ?>
-					<li><a href="guard.list.php">Guard List</a></li>
+					<li><a href="guard.list.php">Change Password</a></li>
 					<li><a href="logout.php">Logout</a></li>
 					<?php } else{ ?>
 					<li><a href="login.php">Login</a></li>
